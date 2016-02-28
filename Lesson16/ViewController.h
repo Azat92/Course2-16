e50f34171d30c5c5291a1e69fd56407e12e2b88d
Azat Almeev <azat.almeev@gmail.com>
2016-02-28 20:48:47 +0300

    Initial Commit

---
+++ Lesson16/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson16
+//
+//  Created by Azat Almeev on 28.02.16.
+//  Copyright © 2016 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
